fa5fdec4ace42efb075381446a18defe32462aa8
Upendra Chaurasia <upendra.chaurasia@outlook.com>
2022-07-15 16:37:21 +0530

    conditionals

---
+++ JavaScript Fundamentals/Day 3 Operator and Conditionals/ifelseif.js
@@ -0,0 +1,9 @@
+let year = prompt("Enter the year",'');
+
+if (year < 2015){
+    alert("Not yet");
+} else if (year > 2015){
+    alert("In the future");
+} else {
+    alert("The year is 2015");
+}
