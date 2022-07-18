009131dbb620ccbad077621a035f46c7274f417b
Upendra Chaurasia <upendra.chaurasia@outlook.com>
2022-07-18 08:18:05 +0530

    added documentation

--- Chai/src/app.js
+++ Chai/src/app.js
@@ -1,16 +1,29 @@
 class Cube {
+   /**
+    * The constructor function is a function that is called when a new object is created.
+    * @param length - The length of the array.
+    */
     constructor(length) {
     this.length = length;
     }
     
+   /**
+    * It returns the length of the side of the square.
+    * @returns The length of the side of the square.
+    */
     getSideLength () {
     return this.length;
     }
     
+   /**
+    * The function getSurfaceArea() returns the surface area of a cube.
+    * @returns The surface area of the cube.
+    */
     getSurfaceArea () {
     return (this.length * this.length) * 6;
     }
     
+  /* Returning the volume of the cube. */
     getVolume () {
     return Math.pow(this.length,3);
     }
